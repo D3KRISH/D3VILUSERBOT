5d730d255e567b35d3cd4a4b0268bbd2c9fa34fc
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-07-01 12:15:38 +0530

    Create ping.py

---
+++ d3vilbot/plugins/ping.py
@@ -0,0 +1,26 @@
+import asyncio
+import datetime
+
+from . import *
+
+@bot.on(d3vil_cmd(pattern="ping$"))
+@bot.on(sudo_cmd(pattern="ping$", allow_sudo=True))
+async def pong(d3vil):
+    if d3vil.fwd_from:
+        return
+    start = datetime.datetime.now()
+    event = await eor(d3vil, "`·.·★ ℘ıŋɠ ★·.·´")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(
+        f"╰•★★  ℘ơŋɠ ★★•╯\n\n    ⚘  `{ms}`\n    ⚘  __**Oɯɳҽɾ**__ **:**  {d3vil_mention}"
+    )
+
+
+CmdHelp("ping").add_command(
+  "ping", None, "Checks the ping speed of your Hêllẞø†"
+).add_warning(
+  "✅ Harmless Module"
+).add()
+
+# hellbot
